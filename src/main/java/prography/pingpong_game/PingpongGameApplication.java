3f2e819cf47ebd813944321507724b4a1a5bd9bc
0sunset0 <chi6465618@naver.com>
2025-02-15 10:57:52 +0900

    first commit

---
+++ src/main/java/prography/pingpong_game/PingpongGameApplication.java
@@ -0,0 +1,13 @@
+package prography.pingpong_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpongGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpongGameApplication.class, args);
+	}
+
+}
