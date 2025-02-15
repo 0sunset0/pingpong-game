1c88aae164ddfa8eb4f393d49c74a7af7f66fe54
0sunset0 <chi6465618@naver.com>
2025-02-15 23:22:45 +0900

    chore: 스프링 버전 이슈로 다운그레이드 (#6)
    @ControllerAdvice 애노테이션과 Swagger 사이에 일어난 충돌이다. @ControllerAdvice가 없다면 Swagger는 controller class를 가져와 API 정의를 분석한다.@ControllerAdvice가 있다면 Swagger 응답이 내가 응답을 통일해 둔 방식으로 변환되고, Swagger 웹 페이지는 응답 구조를 인식할 수 없게 된다.프로젝트의 패키지에서 @ControllerAdvice가 적용될 곳을 명시하면 Swagger 응답이 변환되지 않아 충돌을 해결할 수 있다.

--- src/main/java/prography/pingpong_game/config/OpenApiConfig.java
+++ src/main/java/prography/pingpong_game/config/OpenApiConfig.java
@@ -1,15 +1,13 @@
 package prography.pingpong_game.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(
+        info = @io.swagger.v3.oas.annotations.info.Info(
                 title = "PingPong Game API",
                 description = "프로그라피 10기 과제 (탁구 게임 API) 문서"
         )
 )
-public class SwaggerConfig {
+public class OpenApiConfig {
 }
